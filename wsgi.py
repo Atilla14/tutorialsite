3c9f89b2258843de3d4ee56168cf9d04348c3c45
hrishikesh_rao <hrishikeshrao94@gmail.com>
2015-07-01 04:07:08 +0000

    lol procfile

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gettingstarted project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tutorialpost.settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
