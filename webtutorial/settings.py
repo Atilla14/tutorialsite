1e5dcda25261c634c79a1a96992814f63e6101ce
hrishikesh_rao <hrishikeshrao94@gmail.com>
2015-07-04 09:30:36 +0530

    satic changes

--- webtutorial/settings.py
+++ webtutorial/settings.py
@@ -115,5 +115,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = os.path.join('staticfiles')
 STATIC_URL = '/static/'
